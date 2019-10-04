0c34f0504bd42013c66c9ec68a639d4b0b796416
Johan Baath <info@johanbaath.com>
2019-10-04 09:47:22 +0200

    reformat

--- pages/johan-baath.js
+++ pages/johan-baath.js
@@ -1,47 +1,90 @@
 import { Header } from '../components/header'
 import { InPageHeader } from '../components/inPageHeader'
 
-export default function JohanBaath () {
+export default function JohanBaath() {
   return (
     <>
-      <Header title={`sthlm.io - Johan Bååth`} canonical="/johan-baath/"/>
+      <Header title={`sthlm.io - Johan Bååth`} canonical="/johan-baath/" />
       <section>
         <div className="content">
-          <InPageHeader/>
+          <InPageHeader />
           <h2>Johan Bååth</h2>
           <p>Dalagatan 55, 113 31 Stockholm</p>
-          <small>+46 760 062 560 / <a href="mailto:info@johanbaath.com">info@johanbaath.com</a>
-          <br/>
-          <a href="https://www.linkedin.com/in/johanbaath/">LinkedIn</a> / <a href="https://github.com/johanbaath">GitHub</a>
+          <small>
+            +46 760 062 560 /{' '}
+            <a href="mailto:info@johanbaath.com">info@johanbaath.com</a>
+            <br />
+            <a
+              href="https://www.linkedin.com/in/johanbaath"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              LinkedIn
+            </a>{' '}
+            /{' '}
+            <a
+              href="https://github.com/johanbaath"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              GitHub
+            </a>
           </small>
           <p className="text">
-            I’ve been working as a developer professionally for 10+ years and I enjoy developing complete
-            products from ground up, including the full tech stack and design.
-            <br /><br />
-            The last couple of years I've been dedicated to working almost exclusively with React and Node.js
-            full time in large scale projects.
-            <br /><br />
-            Before working as a consultant I launched a streaming music service focused on electronic music with two friends/co-founders.
-            I was responsible for everything technical including backend/frontend and providing reliable hosting for millions of songs.
-            <br /><br />
-            We secured music licensing from several major music labels such as Sony Music and raised over 5MSEK in funding.
-            <br/><br/>
-            Want to know more about me? <a href="mailto:info@johanbaath.com?subject=Let's meet over coffee!">Let's meet over coffee in Stockholm!</a>
+            I’ve been working as a developer professionally for 10+ years and I
+            enjoy developing complete products from ground up, including the
+            full tech stack and design.
+            <br />
+            <br />
+            The last couple of years I've been dedicated to working almost
+            exclusively with React and Node.js full time in large scale
+            projects.
+            <br />
+            <br />
+            Before working as a consultant I launched a streaming music service
+            focused on electronic music with two friends/co-founders. I was
+            responsible for everything technical including backend/frontend and
+            providing reliable hosting for millions of songs.
+            <br />
+            <br />
+            We secured music licensing from several major music labels such as
+            Sony Music and raised over 5MSEK in funding.
+            <br />
+            <br />
+            Want to know more about me?{' '}
+            <a href="mailto:info@johanbaath.com?subject=Let's meet over coffee!">
+              Let's meet over coffee in Stockholm!
+            </a>
           </p>
 
           <h3>Next dream gig</h3>
-          <p className="text">Serverless, Next.js, TypeScript/Flow, GraphQL, PostgreSQL</p>
+          <p className="text">
+            Serverless, Next.js, TypeScript/Flow, GraphQL, PostgreSQL
+          </p>
 
           <h3>Summary of Qualifications</h3>
           <ul>
             <li>Highly experienced in Node.js</li>
-            <li>Highly experienced in modern JavaScript, including TypeScript and Flow</li>
+            <li>
+              Highly experienced in modern JavaScript, including TypeScript and
+              Flow
+            </li>
             <li>Working professionally with React for 4+ years</li>
             <li>GraphQL and running it in production</li>
-            <li>Databases and search indexes such as PostgreSQL, MongoDB, RethinkDB, Elasticsearch, Solr, Redis</li>
-            <li>Several years of experience running large scale apps in production</li>
-            <li>Running services on Docker, both for development and production environments.</li>
-            <li>DevOps: CI/CD, cloud, physical infrastructure, scaling, automation</li>
+            <li>
+              Databases and search indexes such as PostgreSQL, MongoDB,
+              RethinkDB, Elasticsearch, Solr, Redis
+            </li>
+            <li>
+              Several years of experience running large scale apps in production
+            </li>
+            <li>
+              Running services on Docker, both for development and production
+              environments.
+            </li>
+            <li>
+              DevOps: CI/CD, cloud, physical infrastructure, scaling, automation
+            </li>
           </ul>
 
           <h3>Wants to learn more about</h3>
@@ -52,18 +95,47 @@ export default function JohanBaath () {
           </ul>
 
           <h3>Latest Projects</h3>
-          <h4><a href="https://cabonline.com/">Cabonline Technologies AB</a></h4>
-          <p className="text">The largest taxi network in the Nordics.<br/>Lead Frontend, March 2017 - May 2019</p>
-          <h4><a href="https://inspireme.se/">Inspireme i Sverige AB</a></h4>
-          <p className="text">Online courses by leading experts.<br/>Lead Fullstack/DevOps, January 2017 - March 2017</p>
-          <h4><a href="https://www.svt.se/sport/">SVT Sport</a></h4>
-          <p className="text">One of Sweden's top sports site.<br /> Fullstack/DevOps, September 2015 - Jan 2017</p>
-          <h4><a href="https://iveo.se/">IVEO AB</a></h4>
-          <p className="text">Digital Agency in Stockholm.<br />CTO/Developer, 2014 - 2015</p>
-          <h4><a href="#">Revibe AB</a></h4>
-          <p className="text">Electronic music streaming service & DJ tools.<br/>Fullstack/DevOps/Architect/Co-Founder, 2010 - 2014</p>
+          <h4>
+            <a href="https://cabonline.com/">Cabonline Technologies AB</a>
+          </h4>
+          <p className="text">
+            The largest taxi network in the Nordics.
+            <br />
+            Lead Frontend, March 2017 - May 2019
+          </p>
+          <h4>
+            <a href="https://inspireme.se/">Inspireme i Sverige AB</a>
+          </h4>
+          <p className="text">
+            Online courses by leading experts.
+            <br />
+            Lead Fullstack/DevOps, January 2017 - March 2017
+          </p>
+          <h4>
+            <a href="https://www.svt.se/sport/">SVT Sport</a>
+          </h4>
+          <p className="text">
+            One of Sweden's top sports site.
+            <br /> Fullstack/DevOps, September 2015 - Jan 2017
+          </p>
+          <h4>
+            <a href="https://iveo.se/">IVEO AB</a>
+          </h4>
+          <p className="text">
+            Digital Agency in Stockholm.
+            <br />
+            CTO/Developer, 2014 - 2015
+          </p>
+          <h4>
+            <a href="#">Revibe AB</a>
+          </h4>
+          <p className="text">
+            Electronic music streaming service & DJ tools.
+            <br />
+            Fullstack/DevOps/Architect/Co-Founder, 2010 - 2014
+          </p>
         </div>
-        { /*language=SCSS*/}
+        {/*language=SCSS*/}
         <style jsx>{`
           section {
             width: 100%;
@@ -101,7 +173,7 @@ export default function JohanBaath () {
           }
 
           a:hover {
-            color: #3E46CF;
+            color: #3e46cf;
           }
         `}</style>
       </section>
