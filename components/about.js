458c20ef0d73f5c603ba914524ae03df0b7b2e61
Fredrik Lack <fredrik@sthlm.io>
2024-07-03 08:07:40 +0200

    Update about

--- components/about.js
+++ components/about.js
@@ -22,7 +22,7 @@ export function About() {
             <img alt="kubernetes" src={'/static/kubernetes.png'} />
           </div>
           <div>
-            <img alt="hetzner" src={'/static/hetzner.png'} />
+            <img alt="kafka" src={'/static/kafka.png'} />
           </div>
           <div>
             <img alt="node.js" src={'/static/nodejs.png'} />
@@ -37,7 +37,7 @@ export function About() {
             <img alt="raspberry pi" src={'/static/raspberry-pi.png'} />
           </div>
           <div>
-            <img alt="lets encrypt" src={'/static/letsencrypt.png'} />
+            <img alt="google cloud platform" src={'/static/gcp.png'} />
           </div>
         </div>
       </div>
