e7c66c12384525efc1a3e71ba61c6e2492db344e
Fredrik Lack <fredrik@sthlm.io>
2026-05-03 18:58:12 +0200

    Add Nav and Footer components to _error.js; improve layout and styling for responsiveness

--- pages/_error.js
+++ pages/_error.js
@@ -1,4 +1,6 @@
 import { Header } from '../components/header'
+import { Nav } from '../components/nav'
+import { Footer } from '../components/footer'
 
 export default function Error() {
   return (
@@ -7,74 +9,78 @@ export default function Error() {
         <title>sthlm.io - 404</title>
         <meta name="robots" content="noindex" />
       </Header>
-      <section>
-        <div className="content">
-          <a href="/">
-            <img src={'/static/logo_small.svg'} width="150" />
-          </a>
-          <div>
-            <h1>404</h1>
-            <p>HTTP 404 Not found</p>
+      <div className="page">
+        <Nav />
+        <section>
+          <div className="content">
+            <div>
+              <h1>404</h1>
+              <p>HTTP 404 Not found</p>
+            </div>
           </div>
-        </div>
-        {/*language=SCSS*/}
-        <style jsx>{`
-          section {
-            width: 100%;
-            padding: 0;
-            background: #fff;
-            z-index: 0;
-            position: relative;
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-          }
+        </section>
+        <Footer />
+      </div>
+      {/*language=SCSS*/}
+      <style jsx>{`
+        .page {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
 
-          .content {
-            display: flex;
-            flex-direction: column;
-            margin: 0 auto;
-            width: 1024px;
-            max-width: calc(100% - 40px);
-            padding: 20px;
-            height: calc(100vh - 40px);
-          }
+        section {
+          width: 100%;
+          padding: 0;
+          background: #fff;
+          z-index: 0;
+          position: relative;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          flex: 1;
+        }
 
-          .content div {
-            display: flex;
-            height: 100%;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-          }
+        .content {
+          display: flex;
+          flex-direction: column;
+          margin: 0 auto;
+          width: 1024px;
+          max-width: calc(100% - clamp(28px, 6vw, 40px));
+          padding: 20px 0;
+          flex: 1;
+        }
 
-          a {
-            position: absolute;
-          }
+        .content div {
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
 
+        h1 {
+          font-size: 120px;
+          margin: 0;
+        }
+
+        @media screen and (min-width: 500px) {
           h1 {
-            font-size: 120px;
-            margin: 0;
+            font-size: 200px;
           }
+        }
 
-          @media screen and (min-width: 500px) {
-            h1 {
-              font-size: 200px;
-            }
-          }
+        p {
+          margin-top: 0;
+          width: auto;
+        }
 
+        @media screen and (min-width: 500px) {
           p {
-            margin-top: 0;
-            width: auto;
+            margin-top: -30px;
           }
-
-          @media screen and (min-width: 500px) {
-            p {
-              margin-top: -30px;
-            }
-          }
-        `}</style>
-      </section>
+        }
+      `}</style>
     </>
   )
 }
